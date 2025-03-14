d0a94c172ce0a5450b0fab0c518f8192432b33b3
Anthony S. <tonesto7@gmail.com>
2025-03-14 16:33:08 -0400

    fixes

--- opendbc_repo/opendbc/safety/safety/safety_ford.h
+++ opendbc_repo/opendbc/safety/safety/safety_ford.h
@@ -187,7 +187,7 @@ static void ford_rx_hook(const CANPacket_t *to_push) {
       pcm_cruise_check(cruise_engaged);
       acc_main_on = (cruise_state == 3U) || cruise_engaged;
     }
-
+    
     if (addr == FORD_Steering_Data_FD1) {
       mads_button_press = GET_BIT(to_push, 40U) ? MADS_BUTTON_PRESSED : MADS_BUTTON_NOT_PRESSED;
     }
@@ -353,6 +353,7 @@ static safety_config ford_init(uint16_t param) {
     {.msg = {{FORD_EngBrakeData, 0, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 10U}, { 0 }, { 0 }}},
     {.msg = {{FORD_EngVehicleSpThrottle, 0, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 100U}, { 0 }, { 0 }}},
     {.msg = {{FORD_DesiredTorqBrk, 0, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
+    {.msg = {{FORD_Steering_Data_FD1, 0, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 10U}, { 0 }, { 0 }}},
   };
 
   #define FORD_COMMON_TX_MSGS              \
