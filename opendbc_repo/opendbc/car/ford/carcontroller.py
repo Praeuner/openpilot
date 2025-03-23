223284cd2d66143a14d486e1df1e3dca96d043bd
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-03-22 17:14:34 -0700

    self.path_angle_k_i = 0.01

--- opendbc_repo/opendbc/car/ford/carcontroller.py
+++ opendbc_repo/opendbc/car/ford/carcontroller.py
@@ -150,7 +150,7 @@ class CarController(CarControllerBase):
 
     # path angle PID tuning
     self.path_angle_k_p = 6 # self.path_angle_high_curvature_factor_UI
-    self.path_angle_k_i = 0.0001
+    self.path_angle_k_i = 0.01
     self.path_angle_pid_controller = PIDController(k_p=self.path_angle_k_p, k_i=self.path_angle_k_i, rate=20) # rate in Hz
 
     ## High curvature path_angle / path_offset tuning stars here ##
