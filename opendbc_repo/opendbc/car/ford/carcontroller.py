84f9c247a9b4a2693fb6b44d469011d60f909352
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-03-23 14:36:35 -0700

    self.path_angle_k_p = 3.25    self.enable_lanefull_mode = False

--- opendbc_repo/opendbc/car/ford/carcontroller.py
+++ opendbc_repo/opendbc/car/ford/carcontroller.py
@@ -115,13 +115,13 @@ class CarController(CarControllerBase):
     self.custom_path_offset = -0.1 # updated from UI: applies a custom offset to help with in-lane positioning
     self.lane_width_tolerance_factor = 0.75
     self.min_laneline_confidence_bp = [0.6, 0.8]
-    self.enable_lanefull_mode = True
+    self.enable_lanefull_mode = False
 
     # path angle variables
     self.path_angle_filter_samples = 10 # number of samples to use for the moving average filter
     self.path_angle_deque = deque(maxlen=self.path_angle_filter_samples) # deque to hold the samples
     self.path_angle_wheel_angle_conversion = 0.0017 # degrees to milliradians
-    self.path_angle_k_p = 3.0
+    self.path_angle_k_p = 3.25
     self.path_angle_k_i = 0.05
     self.path_angle_pid_controller = PIDController(k_p=self.path_angle_k_p, k_i=self.path_angle_k_i, rate=20) # rate in Hz
     self.wheel_angle_speed_bp = [11, 15] # what speed to adjust wheel_angle
