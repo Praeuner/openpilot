e7b840a403dbe13587764b49d1d020f1273d8a29
tonesto7 <tonesto7@users.noreply.github.com>
2025-04-10 08:38:34 -0400

    Setup new bluepilot sentry instance

--- system/sentry.py
+++ system/sentry.py
@@ -1,4 +1,5 @@
 """Install exception handler for process crash."""
+
 import os
 import traceback
 from datetime import datetime
@@ -20,7 +21,8 @@ CRASHES_DIR = Paths.crash_log_root()
 
 class SentryProject(Enum):
   # python project
-  SELFDRIVE = "https://186a6736b7927e5ae9b92c869ba81b6b@o1138119.ingest.us.sentry.io/4508660076052480"
+  # SELFDRIVE = "https://186a6736b7927e5ae9b92c869ba81b6b@o1138119.ingest.us.sentry.io/4508660076052480" # Native
+  SELFDRIVE = "https://e4e23da828758b43877bff008866545f@o4509128983117824.ingest.us.sentry.io/4509129015885824"  # BP
   # native project
   SELFDRIVE_NATIVE = SELFDRIVE
 
@@ -54,10 +56,7 @@ def save_exception(content: str) -> None:
     if not os.path.exists(CRASHES_DIR):
       os.makedirs(CRASHES_DIR)
 
-    files = [
-      os.path.join(CRASHES_DIR, datetime.now().strftime("%Y-%m-%d--%H-%M-%S.log")),
-      os.path.join(CRASHES_DIR, "error.log")
-    ]
+    files = [os.path.join(CRASHES_DIR, datetime.now().strftime("%Y-%m-%d--%H-%M-%S.log")), os.path.join(CRASHES_DIR, "error.log")]
 
     for fn in files:
       with open(fn, 'w') as f:
@@ -124,13 +123,9 @@ def init(project: SentryProject) -> bool:
   if project == SentryProject.SELFDRIVE:
     integrations.append(ThreadingIntegration(propagate_hub=True))
 
-  sentry_sdk.init(project.value,
-                  default_integrations=False,
-                  release=get_version(),
-                  integrations=integrations,
-                  traces_sample_rate=1.0,
-                  max_value_length=8192,
-                  environment=env)
+  sentry_sdk.init(
+    project.value, default_integrations=False, release=get_version(), integrations=integrations, traces_sample_rate=1.0, max_value_length=8192, environment=env
+  )
 
   sentry_sdk.set_user({"id": dongle_id, "name": git_username})
   sentry_sdk.set_tag("dirty", build_metadata.openpilot.is_dirty)
