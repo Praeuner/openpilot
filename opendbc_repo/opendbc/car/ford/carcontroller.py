0354d031b104e687c7cff13f1c4a5157c50420e8
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-04-11 17:18:33 -0500

    40/60

--- opendbc_repo/opendbc/car/ford/carcontroller.py
+++ opendbc_repo/opendbc/car/ford/carcontroller.py
@@ -107,7 +107,7 @@ class CarController(CarControllerBase):
     self.lane_change_factor_bp = [4.4, 40.23] # what speed to adjust lane_change_factor
     self.lane_change_factor_low = 0.95 # lane_change_factor at 4.4 m/s
     self.lane_change_factor_high = 0.75 # updated from UI: lane_change_factor at 40.23 m/s
-    self.pc_blend_ratio = 0.60 # 40% Predicted Curvature and 60% Desired Curvature
+    self.pc_blend_ratio = 0.40 # 40% Predicted Curvature and 60% Desired Curvature
 
     # Curvature rate variables
     self.curvature_rate_delta_t = 0.3  # [s] used in denominator for curvature rate calculation
