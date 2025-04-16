92eba301554fde9584bc2b8aaa2ba96aac3c0edf
Blue Genie <8130363+blue-genie@users.noreply.github.com>
2025-04-16 08:08:46 -0500

    Remove Genie's custom connect

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -75,12 +75,6 @@ function launch {
   fi
 
   # custom portal for non-comma devices
-  if grep -q -E "e36b272d5679115f" /data/params/d/DongleId; then
-    export API_HOST=https://genie.snagytech.com
-    export ATHENA_HOST=wss://athena.snagytech.com
-  fi
-
-  # custom portal for non-comma devices
   if grep -q -E "160fab5f43d9e5c1" /data/params/d/DongleId; then
     export API_HOST=https://api.konik.ai
     export ATHENA_HOST=wss://athena.konik.ai
