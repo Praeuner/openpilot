b12a66e849e6e4c58073a95bf8608e1ae92e2d5a
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-04-15 20:39:29 -0500

    self.path_angle_k_p = 3.5

--- opendbc_repo/opendbc/car/ford/carcontroller.py
+++ opendbc_repo/opendbc/car/ford/carcontroller.py
@@ -120,7 +120,7 @@ class CarController(CarControllerBase):
     self.path_angle_filter_samples = 10 # number of samples to use for the moving average filter
     self.path_angle_deque = deque(maxlen=self.path_angle_filter_samples) # deque to hold the samples
     self.path_angle_wheel_angle_conversion = (np.pi/180)/10 # degrees to radians, divide by 10 to reduce the gain
-    self.path_angle_k_p = 2.5
+    self.path_angle_k_p = 3.5
     self.path_angle_k_i = 0.0
     self.path_angle_pid_controller = PIDController(k_p=self.path_angle_k_p, k_i=self.path_angle_k_i, rate=20)
     self.wheel_angle_lookup_time = 0.3
