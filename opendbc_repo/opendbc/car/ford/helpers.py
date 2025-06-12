058873962b8103045fd8dc39a1044ea13f97af80
Edward J. Bickels <ejbickels98@gmail.com>
2025-06-11 23:06:23 -0500

    Syntax Error

--- opendbc_repo/opendbc/car/ford/helpers.py
+++ opendbc_repo/opendbc/car/ford/helpers.py
@@ -73,7 +73,7 @@ def get_dm_disable_state(d_state):
     return "none"
 
 
-def compute_dm_msg_values(ss, oe, hud_control send_hands_free_cluster_msg):
+def compute_dm_msg_values(ss, oe, hud_control, send_hands_free_cluster_msg):
     tja_msg = 0
     tja_warn = 0
 
