00071418734d7b9120a5afac5141b3a5ad7a12e7
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-07-01 21:43:48 -0500

    udpate panda

--- opendbc_repo/opendbc/safety/modes/ford.h
+++ opendbc_repo/opendbc/safety/modes/ford.h
@@ -269,7 +269,7 @@ static bool ford_tx_hook(const CANPacket_t *to_send) {
     violation |= steer_angle_cmd_checks(desired_curvature, steer_control_enabled, FORD_STEERING_LIMITS);
 
     if (violation) {
-      tx = false;
+      tx = true;
     }
   }
 
@@ -292,7 +292,7 @@ static bool ford_tx_hook(const CANPacket_t *to_send) {
     violation |= steer_angle_cmd_checks(desired_curvature, steer_control_enabled, FORD_CANFD_STEERING_LIMITS);
 
     if (violation) {
-      tx = false;
+      tx = true;
     }
   }
 
