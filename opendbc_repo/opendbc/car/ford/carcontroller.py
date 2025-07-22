1f98e46b34ceb4b123d40127fcafb762b89ea51f
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-07-21 20:32:53 -0500

    putting path_angle LC gain back

--- opendbc_repo/opendbc/car/ford/carcontroller.py
+++ opendbc_repo/opendbc/car/ford/carcontroller.py
@@ -147,7 +147,7 @@ class CarController(CarControllerBase):
     # path angle low curvature variables
     self.LC_PID_GAIN_CAN = 5.0
     self.LC_PID_GAIN_CANFD_SMALL_VEHICLE = 3.0
-    self.LC_PID_GAIN_CANFD_LARGE_VEHICLE = 1.5
+    self.LC_PID_GAIN_CANFD_LARGE_VEHICLE = 3.0
     self.LC_PID_GAIN_UI = 0.0 # gain for UI tuning
     self.LC_PID_GAIN = 0.0
     self.LC_PID_k_p = 0.25
