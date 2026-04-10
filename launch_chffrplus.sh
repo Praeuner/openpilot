c6f8350f17c0622d7a075fe03ae760425e68fd2c
alan-polk <56047663+alan-polk@users.noreply.github.com>
2026-04-09 20:28:33 -0500

    Remove Konik Portal for Uncle Tony's C3X

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -83,18 +83,6 @@ function launch {
     export ATHENA_HOST=wss://athena.konik.ai
   fi
 
-   # custom portal for Uncle Tony's C3X (Comma ID)devices
-  if grep -q -E "3b1f3f2a42d70b76" /data/params/d/DongleId; then
-    export API_HOST=https://api.konik.ai
-    export ATHENA_HOST=wss://athena.konik.ai
-  fi
-
-  # custom portal for Uncle Tony's C3X (Konik ID)devices
-  if grep -q -E "78732aa1f2ca0217" /data/params/d/DongleId; then
-    export API_HOST=https://api.konik.ai
-    export ATHENA_HOST=wss://athena.konik.ai
-  fi
-
   # write tmux scrollback to a file
   tmux capture-pane -pq -S-1000 > /tmp/launch_log
 
