a7891da123c33079cbb1ab24f71bde595561a190
Praeuner <lukepraeuner@gmail.com>
2026-09-03 22:12:14 -0500

    Update lateral_angle_ext.py

--- opendbc_repo/opendbc/sunnypilot/car/ford/lateral_angle_ext.py
+++ opendbc_repo/opendbc/sunnypilot/car/ford/lateral_angle_ext.py
@@ -538,11 +538,11 @@ class LateralAngleExt:
       v_ego, [11.18, 31.29], [1.00, (self.path_angle_gain_lowC_highV * self.user_dampening_factor)]
     )
     self.high_gain_calc = interp(
-      v_ego, [11.18, 31.29], [(1.30 * self.low_speed_curv_factor), (1.25 * self.path_angle_gain_highC_highV * self.high_speed_curv_factor)]
+      v_ego, [11.18, 31.29], [(1.35 * self.low_speed_curv_factor), (1.25 * self.path_angle_gain_highC_highV * self.high_speed_curv_factor)]
     )
 
-    # Speed-interpolated curve-radius: at low speed, dont need full gain until a much tighter curve
-    high_gain_boundary = interp(v_ego, [11.18, 17.88, 24.59, 31.29], [0.02, 0.018, 0.0135, 0.0035])
+    # Speed-interpolated curve-radius: lower the speed, tighter the curves. easing into transition
+    high_gain_boundary = interp(v_ego, [8.94, 13.41, 16.54, 31.29], [0.02, 0.0195, 0.018, 0.0035])
 
     # As the curve gets bigger, we will need a little boost to the signal to to not understeer
     self.curvature_factor = interp(abs(kappa_cmd), [0.0005, high_gain_boundary], [self.low_gain_calc, self.high_gain_calc])
